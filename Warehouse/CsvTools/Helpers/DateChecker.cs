6e5c1672cb553cdb6a9ab58d75beddc7ee2e8dfd
Kenneth Christensen <bygdrift@gmail.com>
2022-06-11 10:38:44 +0200

    1.0.4

--- Warehouse/CsvTools/Helpers/DateChecker.cs
+++ Warehouse/CsvTools/Helpers/DateChecker.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 
-namespace Bygdrift.Warehouse.CsvTools.Helpers
+namespace Bygdrift.CsvTools.Helpers
 {
     /// <summary>
     /// A class that verifies if a value is a date
